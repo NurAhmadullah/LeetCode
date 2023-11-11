cb7a7d9489a28828ad1356921bff3f0c229572af
Nur Ahmadullah <54695153+NurAhmadullah@users.noreply.github.com>
2023-11-11 12:41:31 +0600

    Time: 0 ms (100%), Space: 15 MB (94.84%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,53 +11,20 @@
 class Solution {
 public:
 
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(list1 == nullptr){
-            return list2;
-        }
-        if(list2 == nullptr){
-            return list1;
-        }
-        
-        ListNode * sortedList = nullptr;
-
-        if(list1->val < list2->val){
-            sortedList = new ListNode(list1->val);
-            list1 = list1->next;
-        }
-        else{
-            sortedList = new ListNode(list2->val);
-            list2 = list2->next;
-        }
-
-        ListNode * sortedTmp = sortedList;
-
-        while(true){
-            if(list1 != nullptr && list2 != nullptr){
-                if(list1->val < list2->val){
-                    sortedTmp->next = new ListNode(list1->val);
-                    list1 = list1->next;
-                }
-                else{
-                    sortedTmp->next = new ListNode(list2->val);
-                    list2 = list2->next;
-                }
-            }
-            else if(list1 != nullptr){
-                sortedTmp->next = new ListNode(list1->val);
-                list1 = list1->next;
-            }
-            else if(list2 != nullptr){
-                sortedTmp->next = new ListNode(list2->val);
-                list2 = list2->next;
-            }
-            else{
-                break;
-            }
-
-            sortedTmp = sortedTmp->next;
-        }
-
-        return sortedList;
-    }
+     ListNode * mergeTwoLists(ListNode* l1,ListNode* l2){
+		if(l1 == NULL){
+		    return l2;
+		}
+		if (l2 == NULL){
+		    return l1;
+		}
+		if (l1->val < l2->val){
+		    l1->next = mergeTwoLists(l1->next, l2);
+		    return l1;
+		}
+		else{
+		    l2->next = mergeTwoLists(l1, l2->next);
+		    return  l2;
+		}
+	}
 };
